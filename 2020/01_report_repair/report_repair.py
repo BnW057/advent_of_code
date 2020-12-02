f72f90e96893fb6d4cee5769e9b9405a69ecad73
Eris <bnw057@hotmail.com>
2020-12-02 00:56:51 -0500

    Solved second part of code by using a list and a diminishing ceiling. The dictionary approach for the second problem was giving me issues so I opted for the now final solution for the second half of the problem.

--- 2020/01_report_repair/report_repair.py
+++ 2020/01_report_repair/report_repair.py
@@ -22,7 +22,7 @@ def part1():
   d = fillDictionary(__location__)
   result = False
   # Try to find 2020 sum pair in dictionary
-  for val in sorted(d.keys):
+  for val in sorted(d.keys()):
     # Try to use difference of value as key. If True, match is found.
     try:
       result = str(str(2020-int(val)) + "\n") in d;
@@ -38,11 +38,37 @@ def part1():
       break
   print("End of first part.")
 
+def writeAnswer2(one, two, three):
+  f = open(os.path.join(__location__, 'answer2.txt'), "w")
+  f.write("First Value: " + str(one) + "\n")
+  f.write("Second value: " + str(two) + "\n")
+  f.write("Third value: " + str(three) + "\n")
+  f.write("Product: " + str(one * two* three))
+  f.close()
+
 # This part covers the second half of the solution.
 def part2():
-  # We will use the same dictionary, but will try matching three values.
-  d = fillDictionary(__location__)
-  print("Second part")
+  # A dictionary approach fails here. A list has to be used instead.
+  l = []
+  f = open(os.path.join(__location__, 'puzzle_input.txt'), "r")
+  # Map values in file into a list of integers.
+  for line in f:
+    l.append(int(line))
+  # Close file after reading values
+  f.close()
+  # Sort the list.
+  l = sorted(l)
+  for one in l:
+    # Subtract 2020 by the previous value.
+    top_ceiling = 2020-one
+    # Try to find values lower than the top ceiling.
+    for two in l:
+      if two < top_ceiling:
+        mid_ceiling = top_ceiling - two
+        for three in l:
+          if three == mid_ceiling:
+            writeAnswer2(one, two,three)
+  print("End of second part.")
 
 def main():
   part1()
