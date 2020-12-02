733795a0b2bbe108920a036d36d17137f02a5298
Eris <bnw057@hotmail.com>
2020-12-02 00:09:26 -0500

    Since the same information is used for the second half of the problem, I simply refactored the code to use the same execution file and input file, instead of creating a separate py file. This way, I can simply have a main that calls either part1() or part2() functions for the respective half that corresponds to its solution. Some code was modified so that it would be shared among those functions, for the purpose of convenience.

--- 2020/01_report_repair/report_repair.py
+++ 2020/01_report_repair/report_repair.py
@@ -1,33 +1,51 @@
 from os import error, system
 import os
 
-def main():
-  # Initialize variables for later use.
-  d = {};
-  result = False
-  __location__ = os.path.realpath(
+# Working directory location
+__location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def fillDictionary(loc):
+  d = {};
   # Open file with values
-  f = open(os.path.join(__location__, 'puzzle_input.txt'), "r")
+  f = open(os.path.join(loc, 'puzzle_input.txt'), "r")
   # Map values in file into a dictionary
   for line in f:
     d[line] = True;
   # Close file after reading values
   f.close()
+  return d
+
+# This part covers the first half of the solution.
+def part1():
+  # Initialize variables for later use.
+  d = fillDictionary(__location__)
+  result = False
   # Try to find 2020 sum pair in dictionary
-  for val in sorted(d):
+  for val in sorted(d.keys):
     # Try to use difference of value as key. If True, match is found.
     try:
       result = str(str(2020-int(val)) + "\n") in d;
     except error:
       print("An error occurred.")
+    # If match is found, write all respective values to a new file.
     if result == True:
       f = open(os.path.join(__location__, 'answer1.txt'), "w")
       f.write("First Value: " + val)
       f.write("Second value: " + str(2020-int(val)) + "\n")
       f.write("Multipying both:" + str(int(val) * (2020-int(val))))
       f.close()
-      return
-  print("End of execution.")
+      break
+  print("End of first part.")
+
+# This part covers the second half of the solution.
+def part2():
+  # We will use the same dictionary, but will try matching three values.
+  d = fillDictionary(__location__)
+  print("Second part")
+
+def main():
+  part1()
+  part2()
 
 main()
