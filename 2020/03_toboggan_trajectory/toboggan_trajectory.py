bd21dfb8aa42fff4d0ff9d2e79ab1fa9740c1342
Eris <bnw057@hotmail.com>
2020-12-05 01:40:45 -0500

    I had to trim parts of the code because not only was I iterating through every possible initial position to know which position would get the least amount of hits, but I also wasn't even showing the least amount of trees, just the first run. After making the changes and realizing I was doing more work than the problem asked, I redesigned the loop so that it would change its own slope with every traversal of the map, and put the respective values in a list. This way I was able to solve the second part of the problem.

--- 2020/03_toboggan_trajectory/toboggan_trajectory.py
+++ 2020/03_toboggan_trajectory/toboggan_trajectory.py
@@ -8,36 +8,40 @@ def main():
   # Open file to read values
   f = open(os.path.join(__location__, 'puzzle_input.txt'), "r")
   # Initialize loop variable for trying different paths
-  cont = True
-  # Initialize offset variable for different starting points.
-  offset = 0
+  config = [1,1]
   # Initialize positional value. Only this dimension is needed.
   position = 0
   # Initialize trees value. Counts how many trees you hit.
   trees = 0
-  shortest = sys.maxsize
+  hitList = []
   # Extract the length of a line from the file.
   lineLength = len(f.readline())
-  # Enter file read loop
-  while cont:
+  # Try different configurations
+  while config[1] <= 2:
     # Reset the file pointer to beginning
     f.seek(0)
     # Tree counter for every run
     trees = 0
-    position = offset
-    for line in f:
+    position = 0
+    for index, line in enumerate(f):
+      if config[1] == 2 and index % 2 != 0:
+        continue
       # If the position value is larger than the length, modulate.
       # Put the remaining value in the carry.
       if position >= len(line.rstrip("\n")):
         position = position % len(line.rstrip("\n"))
       if line[position] == "#":
         trees+=1;
-      position += 3
-    if shortest > trees:
-      shortest = trees
-    offset+=1
-    if offset == lineLength:
-      cont = not cont
-  print("The number of trees hit is: " + str(trees))
+      position += config[0]
+    print("Current configuration: " + str(config))
+    print("The number of trees hit is: " + str(trees))
+    hitList.append(trees)
+    config[0] += 2
+    if config[1] == 2:
+      break
+    if config[0] > 8:
+      config[0] = 1
+      config[1] = 2
+  print(hitList)
 
 main()
