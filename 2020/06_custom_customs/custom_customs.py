10c34396954e8822fdaa79b2522357cf71ee2139
Eris <bnw057@hotmail.com>
2020-12-10 01:04:52 -0500

    Updated the solution to illustrate the overall idea for the second half of the solution. Solved the second half of the problem.

--- 2020/06_custom_customs/custom_customs.py
+++ 2020/06_custom_customs/custom_customs.py
@@ -1,5 +1,6 @@
 from array import *
 import os
+from types import MemberDescriptorType
 
 __location__ = os.path.realpath(
   os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -9,25 +10,61 @@ def main():
   group_answers = []
   # One list for tracking all least occurrences from all groups
   answer_list = []
+  # Lists for the second part of the problem.
+  temp_list = []
+  corrected_totals = []
+  group_member_total = 0
   # Get file contents
   with open(os.path.join(__location__, 'puzzle_input.txt'), "r") as f:
     # Traverse line by line
     for line in f:
-      # Check if the current char is in answers. If not, add to list.
-      for c in line:
-        if not c in group_answers and c != '\n':
-          group_answers.append(c)
       # If you hit a singular newline, a new group is next.
       # At this point, flush all values into the totals list.
       if line == '\n':
         for i in group_answers:
           answer_list.append(i)
         group_answers = []
+        # Check if the total appearances of a char equals group total
+        temp_list = sorted(temp_list)
+        # List to isolate unique shared answers per group
+        shared_answers = []
+        for j in temp_list:
+          # If pass validation, only add letter once.
+          if (temp_list.count(j) == group_member_total and
+          not j in shared_answers):
+            shared_answers.append(j)
+        if shared_answers != []:
+          for a in shared_answers:
+            corrected_totals.append(a)
+        group_member_total = 0
+        temp_list = []
+        continue
+      # Check if the current char is in answers. If not, add to list.
+      for c in line:
+        if not c in group_answers and c != '\n':
+          group_answers.append(c)
+        if c != '\n':
+          temp_list.append(c)
+      group_member_total += 1
     # For some reason at the very last newline, it simply exits loop.
     # Due to this, one last run executes to flush the list into totals.
+    # The same happens with the solution of the second half.
     if group_answers != []:
       for i in group_answers:
         answer_list.append(i)
-  print("The total of answers is", len(answer_list))
+    if temp_list != []:
+      # List to isolate unique shared answers per group
+      shared_answers = []
+      for j in temp_list:
+        # If pass validation, only add letter once.
+        if (temp_list.count(j) == group_member_total and
+        not j in shared_answers):
+          shared_answers.append(j)
+      if shared_answers != []:
+        for a in shared_answers:
+            corrected_totals.append(a)
+
+  print("The total of answers for the first part is", len(answer_list))
+  print("The total of answers for the second part is", len(corrected_totals))
 
 main()
--- 2020/06_custom_customs/solution_plan.md
+++ 2020/06_custom_customs/solution_plan.md
@@ -5,3 +5,7 @@ For this problem I have to count the total number in which a certain character a
 ## Strategy
 
 I'm not sure how to tackle this one, but I definitely need a list that adds elements to it once per group. I don't think I'd need a 2D array, but the moment one element is added to the list, I have to make sure that it does not mess with another group's validation. If I add 'a' for one group, and then avoid adding more 'a's, the second that I check another group and get an 'a' it would fail if I do not manage to account for the 'a' belonging to another group. Maybe use two separate lists? One I can clear and add elements to check per group, and another one that only adds an occurrence once. After that, I can probably simply count the length of the list because I'd be certain that there would be only repeated elements when they belonged to different groups, and all we care about is the total, so the total of elements would be the answer for the first part of the problem.
+
+## Second part
+
+For the second part, the instruction changes: it's not at least one instance of a character per group, but rather, which letter did every group actually share. For this, I might use a third array and a counter variable. The counter will count how many lines are in a group, and it will become the total number of instances every character must appear within a group. Only those characters will be popped into the answer totals, in a separate list to not mess too much with the code that takes care of the first part.
